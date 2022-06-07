29fc1bf49d03dd0b28458cae0c16cbf18c2da6f1
Garagekeeper <hj06869@naver.com>
2022-06-07 10:45:49 +0900

    쓰이지 않는 함수들 제거
    쓰이지 않는 함수들 제거

--- repository/token.repository.js
+++ repository/token.repository.js
@@ -4,115 +4,14 @@ const redisClient = require("../redis.js");
 const client = require("../redis.js");
 
 const Token = function(token) {
-  this.id = token.id;
-  this.accessToken = token.accessToken;
+  this.userId = token.userId;
+  this.refreshToken = token.refreshToken;
 };
 
 const asyncRedisGet = promisify(redisClient.get).bind(redisClient);
 const asyncRedisDel = promisify(redisClient.del).bind(redisClient);
 const asyncRedisLpop = promisify(redisClient.lPop).bind(redisClient);
 const asyncRedisRpush = promisify(redisClient.rPush).bind(redisClient);
-
-
-
-Token.create = (newToken, results) => {
-    model.Token.create({
-      id : newToken.id,
-      accessToken: newToken.accessToken,
-    }).then(() => {
-        console.log("insert token");
-        results(null, newToken)
-        return;
-      })
-      .catch(err => {
-        console.log(err);
-        results(err,null);
-        return;
-      });
-};
-
-Token.findById = (id, results) => {
-  model.Token.findOne({
-    raw: true,
-    where: {id: id},
-    attribute:['id','accessToken'],
-  })
-  .then(result => {
-    return results(null, result);
-  })
-  .catch(err => {
-    console.log(err);
-    return results(err, null);
-  })
-};
-
-Token.findAllById = (id, results) => {
-  model.Token.findAll({
-    raw: true,
-    where: {id: id},
-    attribute:['id', 'accessToken'],
-  })
-  .then(result => {
-    return results(null, result);
-  })
-  .catch(err => {
-    console.log(err);
-    return results(err, null);
-  })
-};
-
-Token.remove = async (id, accessToken, results) => {
-  await model.Token.destroy({
-    where: {id: id, accessToken: accessToken}
-  })
-  .then(result => {
-    console.log("delete token");
-    return results(null,"done");
-  })
-  .catch(err => {
-    console.log(err);
-    return results(err, null);
-  })
-}
-
-Token.removeOne = (id, results) => {
-  model.Token.findOne({
-    raw: true,
-    where: {id: id},
-    attribute:['id','accessToken'],
-    order: [['createdAt', 'DESC']],
-  })
-  .then(result => {
-    model.Token.destroy({
-      where: {id: result.id, accessToken: result.accessToken}
-    })
-    .catch(err => {
-      console.log(err);
-      return results(err, null);
-    });
-    console.log("delete oldest token");
-    return results(null,"done");
-  })
-  .catch(err => {
-    console.log(err);
-    return results(err, null);
-  })
-
-}
-
-Token.removeAll = (id, results) => {
-  model.Token.destroy({
-    where: {id: id},
-  })
-  .then(result => {
-    console.log("delete all tokens");
-    return results(null,"done");
-  })
-  .catch(err => {
-    console.log(err);
-    return results(err, null);
-  })
-}
 
 Token.findAllRefreshById = (id, results) => {
   redisClient.lRange(id,0,-1).then(result => {
